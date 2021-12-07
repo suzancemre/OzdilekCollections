8c15fe779ebe5c5b37a20045b904f902eb179a2f
testinium <suzancemrealptekin@gmail.com>
2021-12-07 23:35:13 +0300

    first commit

--- src/test/java/ProductDetailPage.java
+++ src/test/java/ProductDetailPage.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-public class UrunDetay extends BaseTest{
+public class UrunDetailPage extends BaseTest{
     @Step("Urun detay <bolum> sayfasında <kontrol> tıklayınca")
     public void urunDetay(String bolum,String kontrol) throws InterruptedException {
         Thread.sleep(1000);
